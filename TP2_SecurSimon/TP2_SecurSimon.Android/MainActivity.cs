1f0ae3cdc065c726b2bc57bfc657a02c9bd910d8
Simondegalile <144238145+Simondegalile@users.noreply.github.com>
2023-09-19 08:28:52 +0200

    IconApp
    Changement d'Icon

--- TP2_SecurSimon/TP2_SecurSimon.Android/MainActivity.cs
+++ TP2_SecurSimon/TP2_SecurSimon.Android/MainActivity.cs
@@ -7,7 +7,7 @@ using Android.OS;
 
 namespace TP2_SecurSimon.Droid
 {
-    [Activity(Label = "TP2_SecurSimon", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
+    [Activity(Label = "TP2_SecurSimon", Icon = "@mipmap/IconApp", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
