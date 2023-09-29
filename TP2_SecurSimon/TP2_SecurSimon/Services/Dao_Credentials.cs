ff01a5b0394aba12287d4cf28af7e678f436f8d6
Simondegalile <144238145+Simondegalile@users.noreply.github.com>
2023-09-29 14:05:36 +0200

    delete ok

--- TP2_SecurSimon/TP2_SecurSimon/Services/Dao_Credentials.cs
+++ TP2_SecurSimon/TP2_SecurSimon/Services/Dao_Credentials.cs
@@ -11,16 +11,13 @@ namespace TP2_SecurSimon.Services
     public class Dao_Credentials
     {
         public List<Credentials> credentialsList;
-
         public Dao_Credentials()
         {
             credentialsList = new List<Credentials>(); // Initialiser la liste
         }
-
         public async Task<List<Credentials>> GetAllCredentialsAsync()
         {
             string url = "http://almeida.alwaysdata.net/getAllCredential";
-
             Uri uri = new Uri(url);
             using (var client = new HttpClient())
             {
@@ -70,9 +67,28 @@ namespace TP2_SecurSimon.Services
         }
         public async Task<bool> DeleteCredentialAsync(int id)
         {
-            return await DeleteCredentials(id);
+            try
+            {
+                Uri uri = new Uri($"http://almeida.alwaysdata.net/deleteCredential/{id}");
+
+                using (var client = new HttpClient())
+                {
+                    HttpResponseMessage response = await client.DeleteAsync(uri).ConfigureAwait(false);
+                    if (response.IsSuccessStatusCode)
+                    {
+                        return true; // Successful deletion
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error in DeleteCredentialAsync: {ex.Message}");
+            }
+            return false; // Deletion failed or an error occurred
         }
 
 
+
+        
     }
 }
